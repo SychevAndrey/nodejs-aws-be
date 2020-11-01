015ed76b3a679db9c160d3da0a5a33dbc000498a
Andrey S <nukemoore@gmail.com>
2020-11-01 17:57:12 +0300

    create-getallproducts-method

--- product-service/handler.ts
+++ product-service/handler.ts
@@ -1,12 +1,29 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import productList from './productList.json';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
-}
+export const getAllProducts: APIGatewayProxyHandler = async (
+	event,
+	_context
+) => {
+	function getAsync() {
+		return new Promise((resolve) => setTimeout(() => resolve(productList), 0));
+	}
+
+	return {
+		statusCode: 200,
+		headers: {
+			'Access-Control-Allow-Headers': 'Content-Type',
+			'Access-Control-Allow-Origin': 'https://d2tvnqhil9jwg2.cloudfront.net',
+			'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+		},
+		body: JSON.stringify(
+			{
+				products: await getAsync(),
+				input: event,
+			},
+			null,
+			2
+		),
+	};
+};
--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -1,44 +1,46 @@
 import type { Serverless } from 'serverless/aws';
 
 const serverlessConfiguration: Serverless = {
-  service: {
-    name: 'product-service',
-    // app and org for use with dashboard.serverless.com
-    // app: your-app-name,
-    // org: your-org-name,
-  },
-  frameworkVersion: '2',
-  custom: {
-    webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
-  },
-  // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack'],
-  provider: {
-    name: 'aws',
-    runtime: 'nodejs12.x',
-    apiGateway: {
-      minimumCompressionSize: 1024,
-    },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-    },
-  },
-  functions: {
-    hello: {
-      handler: 'handler.hello',
-      events: [
-        {
-          http: {
-            method: 'get',
-            path: 'hello',
-          }
-        }
-      ]
-    }
-  }
-}
+	service: {
+		name: 'product-service',
+		// app and org for use with dashboard.serverless.com
+		// app: your-app-name,
+		// org: your-org-name,
+	},
+	frameworkVersion: '2',
+	custom: {
+		bundle: {
+			tsConfig: 'tsconfig.json',
+			sourcemaps: true,
+		},
+	},
+	// Add the serverless-bundle plugin
+	plugins: ['serverless-bundle'],
+	provider: {
+		name: 'aws',
+		region: 'eu-west-1',
+		stage: 'dev',
+		runtime: 'nodejs12.x',
+		apiGateway: {
+			minimumCompressionSize: 1024,
+		},
+		environment: {
+			AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+		},
+	},
+	functions: {
+		getAllProducts: {
+			handler: 'handler.getAllProducts',
+			events: [
+				{
+					http: {
+						method: 'get',
+						path: 'products',
+					},
+				},
+			],
+		},
+	},
+};
 
 module.exports = serverlessConfiguration;
