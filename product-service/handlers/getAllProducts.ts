2da405e05df94666705214249e799f442c052f4e
Andrey S <nukemoore@gmail.com>
2020-11-08 08:10:59 +0300

    headers and tabs

--- product-service/handlers/getAllProducts.ts
+++ product-service/handlers/getAllProducts.ts
@@ -1,29 +1,26 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
-import productList from '../productList.json';
+import { APIGatewayProxyHandler } from "aws-lambda";
+import "source-map-support/register";
+import productList from "../productList.json";
+import { getHeaders, HTTPMethods } from "../../utils/response-headers";
 
 export const getAllProducts: APIGatewayProxyHandler = async (
-	event,
-	_context
+  event,
+  _context
 ) => {
-	function getAsync() {
-		return new Promise((resolve) => setTimeout(() => resolve(productList), 0));
-	}
+  function getAsync() {
+    return new Promise((resolve) => setTimeout(() => resolve(productList), 0));
+  }
 
-	return {
-		statusCode: 200,
-		headers: {
-			'Access-Control-Allow-Headers': 'Content-Type',
-			'Access-Control-Allow-Origin': 'https://d2tvnqhil9jwg2.cloudfront.net',
-			'Access-Control-Allow-Methods': 'GET',
-		},
-		body: JSON.stringify(
-			{
-				products: await getAsync(),
-				input: event,
-			},
-			null,
-			2
-		),
-	};
+  return {
+    statusCode: 200,
+    headers: getHeaders([HTTPMethods.GET]),
+    body: JSON.stringify(
+      {
+        products: await getAsync(),
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
 };
--- product-service/handlers/getProduct.ts
+++ product-service/handlers/getProduct.ts
@@ -1,31 +1,24 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
-import productList from '../productList.json';
+import { APIGatewayProxyHandler } from "aws-lambda";
+import "source-map-support/register";
+import productList from "../productList.json";
+import { getHeaders, HTTPMethods } from "../../utils/response-headers";
 
 export const getProduct: APIGatewayProxyHandler = async (event, _context) => {
-	const { productId } = event.pathParameters;
-	const product = productList.find(
-		({ id }) => event.pathParameters.productId === id
-	);
+  const { productId } = event.pathParameters;
+  const product = productList.find(
+    ({ id }) => event.pathParameters.productId === id
+  );
 
-	if (product !== null && product !== undefined) {
-		return {
-			statusCode: 200,
-			headers: {
-				'Access-Control-Allow-Headers': 'Content-Type',
-				'Access-Control-Allow-Origin': 'https://d2tvnqhil9jwg2.cloudfront.net',
-				'Access-Control-Allow-Methods': 'GET',
-			},
-			body: JSON.stringify(product, null, 2),
-		};
-	}
-	return {
-		statusCode: 400,
-		headers: {
-			'Access-Control-Allow-Headers': 'Content-Type',
-			'Access-Control-Allow-Origin': 'https://d2tvnqhil9jwg2.cloudfront.net',
-			'Access-Control-Allow-Methods': 'GET',
-		},
-		body: JSON.stringify(`Product with ID:${productId} is not found`),
-	};
+  if (!product) {
+    return {
+      statusCode: 404,
+      headers: getHeaders([HTTPMethods.GET]),
+      body: JSON.stringify(`Product with ID:${productId} is not found`),
+    };
+  }
+  return {
+    statusCode: 200,
+    headers: getHeaders([HTTPMethods.GET]),
+    body: JSON.stringify(product, null, 2),
+  };
 };
