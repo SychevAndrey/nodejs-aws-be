7381e555188c1a436c64ef163ca42e5c83ab7eab
Andrey S <nukemoore@gmail.com>
2020-11-16 08:15:32 +0300

    create importProductsFile lambda

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -1,44 +1,67 @@
-import type { Serverless } from 'serverless/aws';
+import type { Serverless } from "serverless/aws";
+import SETTINGS from "./constants";
 
 const serverlessConfiguration: Serverless = {
   service: {
-    name: 'import-service',
+    name: "import-service",
     // app and org for use with dashboard.serverless.com
     // app: your-app-name,
     // org: your-org-name,
   },
-  frameworkVersion: '2',
+  frameworkVersion: "2",
   custom: {
     webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
   },
   // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack'],
+  plugins: ["serverless-webpack"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs12.x',
+    name: "aws",
+    runtime: "nodejs12.x",
+    stage: "dev",
+    region: SETTINGS.awsRegion,
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: "s3:ListBucket",
+        Resource: [`arn:aws:s3:::${SETTINGS.bucketName}`],
+      },
+      {
+        Effect: "Allow",
+        Action: ["s3:*"],
+        Resource: [`arn:aws:s3:::${SETTINGS.bucketName}/*`],
+      },
+    ],
     apiGateway: {
       minimumCompressionSize: 1024,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
     },
   },
   functions: {
-    hello: {
-      handler: 'handler.hello',
+    importProductsFile: {
+      handler: "handler.importProductsFile",
       events: [
         {
           http: {
-            method: 'get',
-            path: 'hello',
-          }
-        }
-      ]
-    }
-  }
-}
+            method: "get",
+            path: "import",
+            cors: true,
+            request: {
+              parameters: {
+                querystrings: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      ],
+    },
+  },
+};
 
 module.exports = serverlessConfiguration;
