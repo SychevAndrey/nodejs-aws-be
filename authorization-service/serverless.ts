d0c11929f9f317350d635a87d4472ac32d6a6f89
Andrey S <nukemoore@gmail.com>
2020-11-30 11:50:50 +0300

    add basic authorizer

--- authorization-service/serverless.ts
+++ authorization-service/serverless.ts
@@ -1,44 +1,48 @@
-import type { Serverless } from 'serverless/aws';
+import type { Serverless } from "serverless/aws";
 
 const serverlessConfiguration: Serverless = {
   service: {
-    name: 'authorization-service',
+    name: "authorization-service",
     // app and org for use with dashboard.serverless.com
     // app: your-app-name,
     // org: your-org-name,
   },
-  frameworkVersion: '2',
+  frameworkVersion: "2",
   custom: {
     webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
   },
   // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack'],
+  plugins: ["serverless-webpack"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs12.x',
+    name: "aws",
+    region: "eu-west-1",
+    stage: "dev",
+    runtime: "nodejs12.x",
     apiGateway: {
       minimumCompressionSize: 1024,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
     },
   },
   functions: {
-    hello: {
-      handler: 'handler.hello',
-      events: [
-        {
-          http: {
-            method: 'get',
-            path: 'hello',
-          }
-        }
-      ]
-    }
-  }
-}
+    basicAuthorizer: {
+      handler: "handler.basicAuthorizer",
+    },
+  },
+  resources: {
+    Resources: {},
+    Outputs: {
+      basicAuthorizerArn: {
+        Value: {
+          "Fn::GetAtt": ["BasicAuthorizerLambdaFunction", "Arn"],
+        },
+      },
+    },
+  },
+};
 
 module.exports = serverlessConfiguration;
